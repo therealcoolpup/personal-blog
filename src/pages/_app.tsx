86276bd84341f0ac2c2e1ddf5208629e06813f13
swagoverlord <azcooldude8@gmail.com>
2023-03-20 00:24:32 +0100

    Installed tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
-import type { AppProps } from 'next/app'
+import "../styles/main.scss"
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>Hello world</p>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
