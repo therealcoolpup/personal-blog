fbcb0c6243112d570ae2ec829abe32ce9e91704c
swagoverlord <azcooldude8@gmail.com>
2023-03-28 01:30:48 +0200

    Setup primary and secondary colours for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pri': '#027efb',
+        'sec': '#696969'
+      }
+    },
   },
   plugins: [],
 }
