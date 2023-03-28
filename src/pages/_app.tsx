54ba449364bb09942090d31772c4f7bf6c192777
swagoverlord <azcooldude8@gmail.com>
2023-03-28 04:02:58 +0200

    Installed apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,26 @@ import "../styles/main.scss"
 import type { AppProps } from 'next/app';
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'https://blog-backend.azaber.com/graphql', // replace with your GraphQL API endpoint
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Nav />
-      <main className="flex flex-col justify-center items-center">
-        <Component {...pageProps} />
+      <main className="flex flex-col justify-center items-center w-5/6 mx-auto my-3">
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
       </main>
       <Footer />
     </div>
