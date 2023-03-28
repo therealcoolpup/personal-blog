d06f10080d3f11f6db22c7ada0604c6722fac219
swagoverlord <azcooldude8@gmail.com>
2023-03-28 04:15:38 +0200

    Update Post.ts

--- src/interfaces/Post.ts
+++ src/interfaces/Post.ts
@@ -2,6 +2,7 @@ export interface Post {
     id: Number;
     title: String;
     content: String;
+    featuredImgAlt: String;
     featuredImgUrl: String;
     category: String;
     datePosted: String;
