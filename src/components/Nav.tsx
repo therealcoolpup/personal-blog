a4e7b22e61e975973fc93b0c351c79ba661d2ab0
swagoverlord <azcooldude8@gmail.com>
2023-04-03 22:57:17 +0200

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,27 +4,28 @@ import Image from 'next/image'
 
 const Nav = () => {
   return (
-    <nav className='p-3'>
-      <ul className='flex flex-row justify-between items-center h-16'>
-      <div>
-        <li>
-          <Link href="/">
-            <Image
-            className='object-contain'      
-              src="/assets/images/logo.webp"
-              alt="Logo"
-              width={100}
-              height={100}
-            />
-          </Link>
-        </li>
-      </div>
-      <div className='flex flex-row gap-6 items-center'>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/posts">Posts</Link></li>
-        <li className='btn-sec'><Link href="/contact">Contact</Link></li>
-      </div>
-      </ul>
+    <nav className='p-3 fixed top-0 left-0 right-0 z-50 bg-white flex flex-row justify-between items-center'>
+      
+        <div>
+          
+            <Link href="/">
+              <Image
+              className='object-contain'      
+                src="/assets/images/logo.webp"
+                alt="Logo"
+                width={100}
+                height={100}
+              />
+            </Link>
+          
+        </div>
+        <div className='flex flex-row gap-6 items-center hidden phone:flex phone:visible'>
+          <><Link href="/about">About</Link></>
+          <><Link href="/posts">Posts</Link></>
+          <Link className='btn-sec' href="/contact">Contact</Link>
+          <><button className='btn toggle-mode'>TOGGLE</button></>
+        </div>
+      
     </nav>
   )
 }
