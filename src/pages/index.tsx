8a5d1d4f21f70e0ab048cf363ab2073fe42060c8
swagoverlord <azcooldude8@gmail.com>
2023-04-15 23:39:48 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
-import Head from 'next/head'
-import { useQuery, gql } from '@apollo/client'
-import { Post } from '@/interfaces/Post'
-import Carousel from 'react-bootstrap/Carousel';
-import Link from 'next/link';
+import Head from "next/head";
+import { useQuery, gql } from "@apollo/client";
+import { Post } from "@/interfaces/Post";
+import Carousel from "react-bootstrap/Carousel";
+import Link from "next/link";
 
 const RECENT_POSTS_QUERY = gql`
   query RecentPosts {
-    posts(first: 3, where: {status: PUBLISH}) {
+    posts(first: 3, where: { status: PUBLISH }) {
       edges {
         node {
-          
           postId
           title
           content
@@ -32,21 +31,20 @@ const RECENT_POSTS_QUERY = gql`
 `;
 
 const Home = () => {
-  const { loading, error, data } = useQuery(RECENT_POSTS_QUERY)
+  const { loading, error, data } = useQuery(RECENT_POSTS_QUERY);
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   const posts: Post[] = data.posts.edges.map(({ node }) => ({
     id: node.postId,
     title: node.title,
     content: node.content,
-    featuredImgAlt: node.featuredImage?.node.altText || '',
-    featuredImgUrl: node.featuredImage?.node.mediaItemUrl || '',
-    category: node.categories.nodes[0]?.name || '',
+    featuredImgAlt: node.featuredImage?.node.altText || "",
+    featuredImgUrl: node.featuredImage?.node.mediaItemUrl || "",
+    category: node.categories.nodes[0]?.name || "",
     datePosted: node.date,
   }));
-
 
   return (
     <>
@@ -56,21 +54,27 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='w-full'> 
-        <Carousel fade interval={1500} controls={false}>         
+      <div className="w-full">
+        <Carousel fade interval={1500} controls={false}>
           {posts.map(post => (
             <Carousel.Item key={post.id}>
-              <div className='flex flex-col justify-center items-center h-[70vh] phone:h-[80vh]'>
+              <div className="flex flex-col justify-center items-center h-[70vh] phone:h-[80vh]">
                 {post.featuredImgUrl && (
-                  <div className='relative w-full'>
-                    <img className='w-full object-cover h-[70vh] phone:h-auto' src={post.featuredImgUrl} alt={post.featuredImgAlt} />
+                  <div className="relative w-full">
+                    <img
+                      className="w-full object-cover h-[70vh] phone:h-auto"
+                      src={post.featuredImgUrl}
+                      alt={post.featuredImgAlt}
+                    />
                     <div className="overlay"></div>
                   </div>
                 )}
               </div>
               <Carousel.Caption>
-                <div className='h-56 flex justify-start items-start'>
-                  <h3 className='text-white'><Link href={`/posts/${post.id}`}>{post.title}</Link></h3>
+                <div className="h-56 flex justify-start items-start">
+                  <h3 className="text-white">
+                    <Link href={`/posts/${post.id}`}>{post.title}</Link>
+                  </h3>
                 </div>
               </Carousel.Caption>
             </Carousel.Item>
@@ -78,7 +82,7 @@ const Home = () => {
         </Carousel>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
