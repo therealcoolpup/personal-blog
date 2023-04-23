4eac2ac2f54018c6c68bb170d1fe2d01ce0d9b44
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-23 15:59:56 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,23 @@
-import "../styles/main.scss"
-import type { AppProps } from 'next/app';
+import "../styles/main.scss";
+import type { AppProps } from "next/app";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
-import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  ApolloProvider,
+} from "@apollo/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const httpLink = createHttpLink({
-  uri: 'https://blog-backend.azaber.com/graphql', // replace with your GraphQL API endpoint
+  uri: "https://blog-backend.azaber.com/graphql", // replace with your GraphQL API endpoint
 });
 
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
 });
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -27,5 +30,5 @@ export default function App({ Component, pageProps }: AppProps) {
       </main>
       <Footer />
     </div>
-  )
+  );
 }
