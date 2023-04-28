3c2ac80a3586223809e699ab3a141cce2fe26d8f
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-28 13:56:35 +0200

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          src="https://kit.fontawesome.com/1e1f15503e.js"
-          crossorigin="anonymous"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
